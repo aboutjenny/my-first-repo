8e702090d10e708d33da4311a74606871ffec833
aboutjenny <Jenny@Jennys-MacBook-Pro.local>
2017-11-09 21:37:39 +0000

    Added JavaScript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Page has loaded! I can´t believe it!");
+
+function helloWorld() {
+    console.log('Hello Wordl!');
+  }
+
+function buttonThree () {
+  document.getElementById ('heading-two').innerHTML = "Hello World";
+  console.log("button two pressed");
+}
